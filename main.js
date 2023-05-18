7f3c36f82dfa1961ebe98749a6948a5c71cad816
Blackout03 <gardner.hayden03@gmail.com>
2023-05-18 10:43:21 +1000

    v3.0.0

--- main.js
+++ main.js
@@ -1,106 +1,92 @@
-const {
-    app,
-    dialog,
-    BrowserWindow,
-    Menu,
-    MenuItem,
-    ipcMain,
-    nativeTheme
-} = require('electron')
-
-const DiscordRPC = require('discord-rpc');
-let rpc
-
-const {
-    autoUpdater
-} = require('electron-updater');
-let updateAv = false;
-
+const { app, dialog, BrowserWindow, Menu, MenuItem } = require('electron')
 const path = require('path')
-
+const DiscordRPC = require('discord-rpc');
+const { autoUpdater } = require('electron-updater');
 
 let pluginName
+var win
+let rpc
+let updateAv = false;
+
+// START OF FLASH PLAYER
+
 switch (process.platform) {
-case 'win32':
-    switch (process.arch) {
-    case 'ia32':
-        pluginName = 'flash/pepflashplayer32_32_0_0_303.dll'
-        break
-    case 'x32':
-        pluginName = 'flash/pepflashplayer32_32_0_0_303.dll'
-        break
-    case 'x64':
-        pluginName = 'flash/pepflashplayer64_32_0_0_303.dll'
-        break
-    }
-    break
-case 'darwin':
-    pluginName = 'flash/PepperFlashPlayer.plugin'
-    break
-case 'linux':
-    app.commandLine.appendSwitch('no-sandbox')
-    pluginName = 'flash/libpepflashplayer.so'
-    break
+	case 'win32':
+		switch (process.arch) {
+			case 'ia32':
+				pluginName = 'flash/pepflashplayer32_32_0_0_303.dll'
+				break
+			case 'x32':
+				pluginName = 'flash/pepflashplayer32_32_0_0_303.dll'
+				break
+			case 'x64':
+				pluginName = 'flash/pepflashplayer64_32_0_0_303.dll'
+				break
+		}
+		break
+	case 'darwin':
+		pluginName = 'flash/PepperFlashPlayer.plugin'
+		break
+	case 'linux':
+		app.commandLine.appendSwitch('no-sandbox')
+		pluginName = 'flash/libpepflashplayer.so'
+		break
 }
 app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName));
 
-var win
+// END OF FLASH PLAYER
+
+// START OF DEEP LINK
+
+app.setAsDefaultProtocolClient('penguinzone')
+
+app.on('open-url', (event, url) => {
+	event.preventDefault();
+});
+
+// END OF DEEP LINK
+
+// START OF CREATE WINDOW
+
 app.on('ready', () => {
-    createWindow();
+	createWindow();
 })
 
-//window creation function
-function createWindow() {
-    win = new BrowserWindow
-    ({
-        title: "Penguin Zone",
-        webPreferences: {
-            plugins: true,
-            nodeIntegration: true
-        },
-        width: 1385,
-        height: 840
-    });
+const createWindow = () => {
+	win = new BrowserWindow({
+		title: "Penguin Zone",
+		webPreferences: {
+			plugins: true,
+			nodeIntegration: true
+		},
+		width: 1385,
+		height: 840
+	})
+
     makeMenu();
-    activateRPC();
-	
-    win.loadURL('https://penguinzone.ca/');
-    autoUpdater.checkForUpdatesAndNotify();
     Menu.setApplicationMenu(fsmenu);
-	
-    win.on('closed', () => {
-    	win = null;
-    });
+
+	updateDiscordRPC()
+
+	win.loadURL('https://penguinzone.ca/');
+
+	autoUpdater.checkForUpdatesAndNotify();
+
+	win.on('closed', () => {
+		win = null;
+	});
 }
 
-// start of menubar part
+// END OF CREATE WINDOW
 
-const aboutMessage = `Penguin Zone Client | v${app.getVersion()}\nThe Owner of Penguin Zone is Mjjrrb905.\n\nClient was created by Random with much code provided by Allinol for use with Coastal Freeze and Snowy Fields.`;
+// START OF MENU
 
+const aboutMessage = `Penguin Zone Client v${app.getVersion()}
+					\nCreated by Random with much code provided by Allinol for use with Coastal Freeze and Snowy Fields.
+					\nMany edits by Blackout for Penguin Zone.
+					\nThe Founders of Penguin Zone are Mjjrrb905 and limegreenicy.`;
 
-function activateRPC() { 
-  const clientId = '1047486604035498055'; 
-  DiscordRPC.register(clientId);
-  rpc = new DiscordRPC.Client({ transport: 'ipc' }); 
-  const startTimestamp = new Date();
-  rpc.on('ready', () => {
-    rpc.setActivity({
-      details: `www.penguinzone.ca`, 
-      startTimestamp, 
-      largeImageKey: `penguin_zone_logo`,
-      largeImageText: `Penguin Zone Client | v${app.getVersion()}`,
-      buttons: [
-          {
-              url: `https://penguinzone.ca/`,
-              label: `Penguin Zone`
-          }
-      ]
-    });
-  });
-  rpc.login({ clientId }).catch();
-}
-
-function makeMenu() { // credits to random
+function makeMenu() { // credits to random, some changes by Blackout for Penguin Zone
     fsmenu = new Menu();
     if (process.platform == 'darwin') {
         fsmenu.append(new MenuItem({
@@ -133,12 +119,14 @@ function makeMenu() { // credits to random
                 },
                 {
                     label: 'Reload',
+                    accelerator: 'CmdOrCtrl+R',
                     click: () => {
                         reload();
                     }
                 },
                 {
                     label: 'Clear Cache',
+					accelerator: 'CmdOrCtrl+Shift+C',
                     click: () => {
                         clearCache();
                         win.loadURL('https://penguinzone.ca/');
@@ -175,12 +163,14 @@ function makeMenu() { // credits to random
         }));
         fsmenu.append(new MenuItem({
             label: 'Reload',
+			accelerator: 'CmdOrCtrl+R',
             click: () => {
                 reload();
             }
         }));
         fsmenu.append(new MenuItem({
             label: 'Clear Cache',
+			accelerator: 'CmdOrCtrl+Shift+C',
             click: () => {
                 clearCache();
                 win.loadURL('https://penguinzone.ca/');
@@ -199,67 +189,96 @@ function clearCache() {
     ses.clearCache(() => {});
 }
 
-// end of menubar
+// END OF MENU
 
-//Auto update part
+// START OF DISCORD RPC
+
+function updateDiscordRPC() { 
+	const clientId = '1047486604035498055'; 
+	DiscordRPC.register(clientId);
+	rpc = new DiscordRPC.Client({ transport: 'ipc' }); 
+	const startTimestamp = new Date();
+	rpc.login({ clientId }).catch();
+
+	win.webContents.on('did-navigate', (event, url) => {
+		let details = 'Staring at the Home Page!';
+
+		if (url === 'https://penguinzonetesting.github.io/events/') {
+			details = 'Checking out Upcoming Events';
+		} else if (url === 'https://penguinzonetesting.github.io/play/') {
+			details = 'Waddling around the World!';
+		}
+
+		rpc.setActivity({
+			details,
+			startTimestamp,
+			largeImageKey: 'penguin_zone_logo',
+			largeImageText: `Penguin Zone Client | v${app.getVersion()}`,
+			buttons: [
+				{
+					url: `https://penguinzone.ca/download`,
+					label: `Download Client`
+				},
+				{
+					url: `penguinzone://`,
+					label: `Launch Client`
+				}
+			]
+		});
+	});
+}
+
+// END OF DISCORD RPC
+
+// START OF AUTO UPDATER
 
 autoUpdater.on('update-available', (updateInfo) => {
 	switch (process.platform) {
-	case 'win32':
-	    dialog.showMessageBox({
-		  type: "info",
-		  buttons: ["Ok"],
-		  title: "Update Available",
-		  message: "There is a new version available (v" + updateInfo.version + "). It will be installed when the app closes."
-	    });
-	    break
-	case 'darwin':
-	    dialog.showMessageBox({
-		  type: "info",
-		  buttons: ["Ok"],
-		  title: "Update Available",
-		  message: "There is a new version available (v" + updateInfo.version + "). Please go install it manually from the website."
-	    });
-	    break
-	case 'linux':
-	    dialog.showMessageBox({
-		  type: "info",
-		  buttons: ["Ok"],
-		  title: "Update Available",
-		  message: "There is a new version available (v" + updateInfo.version + "). Auto-update has not been tested on this OS, so if after relaunching app this appears again, please go install it manually."
-	    });
-	    break
+		case 'win32':
+			dialog.showMessageBox({
+				type: 'info',
+				buttons: ["Ok"],
+				title: "Update Available",
+				message: "There is a new version available (v" + updateInfo.version + "). It will be installed when the app closes."
+			});
+			break
+		case 'win32':
+			dialog.showMessageBox({
+				type: 'info',
+				buttons: ["Ok"],
+				title: "Update Available",
+				message: "There is a new version available (v" + updateInfo.version + "). Please go install it manually from the website."
+			});
+			break
+		case 'win32':
+			dialog.showMessageBox({
+				type: 'info',
+				buttons: ["Ok"],
+				title: "Update Available",
+				message: "There is a new version available (v" + updateInfo.version + "). Auto-update has not been tested on this OS, so if after relaunching app this appears again, please go install it manually."
+			});
+			break
 	}
-    //win.webContents.send('update_available', updateInfo.version);
-});
+})
 
 autoUpdater.on('update-downloaded', () => {
-    updateAv = true;
-});
-
-/*
-ipcMain.on('app_version', (event) => {
-    event.sender.send('app_version', {
-        version: app.getVersion()
-    });
-});
-ipcMain.on('restart_app', () => {
-    autoUpdater.quitAndInstall();
-});
-// end of Auto update part*/
+	updateAv = true;
+})
 
 app.on('window-all-closed', () => {
 	if (updateAv) {
 		autoUpdater.quitAndInstall();
-	}
-	else
-	{
+	} else {
 		if (process.platform !== 'darwin') {
-			app.quit();
+			app.quit()
 		}
 	}
-});
+})
+
+// END OF AUTO UPDATER
 
 app.on('activate', () => {
-  if (win === null) {createWindow();}
+	if (win === null) {
+		createWindow();
+	}
 });
